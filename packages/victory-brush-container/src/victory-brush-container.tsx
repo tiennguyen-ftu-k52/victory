efb3b1ce14f1306ed4f29164bde9a82afac49541
Tien Nguyen <tiennguyen.ftu.k52@gmail.com>
2023-09-11 11:45:14 +0700

    fix linting

--- packages/victory-brush-container/src/victory-brush-container.tsx
+++ packages/victory-brush-container/src/victory-brush-container.tsx
@@ -196,7 +196,8 @@ export const brushContainerMixin = <TBase extends Constructor>(base: TBase) =>
         top: top && assign({ x: top.x1, y: top.y1 }, yProps),
         bottom: bottom && assign({ x: bottom.x1, y: bottom.y1 }, yProps),
         left: left && assign({ y: left.y1, x: left.x1 - handleWidth }, xProps),
-        right: right && assign({ y: right.y1, x: right.x1 + handleWidth }, xProps),
+        right:
+          right && assign({ y: right.y1, x: right.x1 + handleWidth }, xProps),
       };
       const handles = ["top", "bottom", "left", "right"].reduce(
         (memo, curr) => {
